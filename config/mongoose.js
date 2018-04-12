52b5a2b218c916ea982a8b9160d798879999ba9d
Bohdan <bohdansharipov@gmail.com>
2018-04-12 13:21:12 -0400

    Updated configurations.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,17 @@
-// Load the module dependencies
+﻿// Load the module dependencies
 const config = require('./config');
 const mongoose = require('mongoose');
 
 // Define the Mongoose configuration method
-module.exports = function() {
-	// Use Mongoose to connect to MongoDB
-	const db = mongoose.connect(config.db);
+module.exports = function () {
+    // Use Mongoose to connect to MongoDB
+    const db = mongoose.connect(config.db);
 
-	// Load the 'User' model 
-	require('../app/models/user.server.model');
-	require('../app/models/student.server.model');
-	require('../app/models/course.server.model');
+    // Load the 'User' model 
+    require('../app/models/user.server.model');
+    require('../app/models/student.server.model');
+    require('../app/models/course.server.model');
 
-	// Return the Mongoose connection instance
-	return db;
+    // Return the Mongoose connection instance
+    return db;
 };
